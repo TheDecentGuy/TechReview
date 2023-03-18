1e17c1ba94687182708891f27c669cf2dbea3784
TheDecentGuy <vrushabhgawde143@gmail.com>
2023-03-18 22:24:43 +0530

    Format Changes in Result.html

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 import openai
 app = Flask(__name__)
-openai.api_key = "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"
+# openai.api_key = "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"
 
 
 @app.route('/')
 def index():
-    return render_template('home.html', author="king")
+    return render_template('home.html', author="vrushabh")
 
 
 @app.route('/generate', methods=['POST'])
@@ -21,14 +21,34 @@ def generate():
 
     prompt = str(prompt)
 
-    openai.api_key = "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"
+    openai.api_key = "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"
 
     completion = openai.ChatCompletion. create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt+". It should be in proper IEEE format."}])
-    paper = completion.choices[0].message.content
+        model="gpt-3.5-turbo", messages=[{"role": "system", "content": "write abstract on "+title}])
+    abstract = completion.choices[0].message.content
+
+    completion = openai.ChatCompletion. create(
+        model="gpt-3.5-turbo", messages=[{"role": "system", "content": "write introduction on "+title}])
+    intro = completion.choices[0].message.content
+
+    completion = openai.ChatCompletion. create(
+        model="gpt-3.5-turbo", messages=[{"role": "system", "content": "write related work on "+title}])
+    work = completion.choices[0].message.content
+
+    completion = openai.ChatCompletion. create(
+        model="gpt-3.5-turbo", messages=[{"role": "system", "content": "write Proposed Methodology on "+title}])
+    methodology = completion.choices[0].message.content
+
+    completion = openai.ChatCompletion. create(
+        model="gpt-3.5-turbo", messages=[{"role": "system", "content": "write conclusion on "+title}])
+    conclusion = completion.choices[0].message.content
+
+    completion = openai.ChatCompletion. create(
+        model="gpt-3.5-turbo", messages=[{"role": "system", "content": "write analysis on "+title}])
+    anlysis = completion.choices[0].message.content
 
     # Return the generated paper to the user
-    return render_template('result.html', paper=paper, prompt=prompt)
+    return render_template('result.html', title=title, author=author, abstract=abstract, result=result, intro=intro, work=work, methodology=methodology, prompt=prompt, conclusion=conclusion)
 
 
 if __name__ == '__main__':
@@ -36,8 +56,8 @@ if __name__ == '__main__':
 
 
 # import openai
-# openai.api_key = "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"
+# openai.api_key = "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"
 # completion = openai.ChatCompletion. create(
-#     model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Write a technical review paper titled 'smart parking system' by vrushabh that completed. It should be in proper format"}])
+#     model="gpt-3.5-turbo", messages=[{"role": "system", "content": "Write a technical review paper titled 'smart parking system' by vrushabh that completed. It should be in proper format"}])
 # paper = completion.choices[0].message.content
 # print(paper)
