70d4bc6e1b3d8f4421ecc19854ceec1c86812f66
TheDecentGuy <vrushabhgawde143@gmail.com>
2023-04-15 18:30:23 +0530

    IEEE output word document generated successfully.

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def generate():
     print(prompt)
     prompt = str(prompt)
 
-    openai.api_key = "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"
+    openai.api_key = "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"
 
     completion = openai.ChatCompletion. create(
         model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Abstract based on "+pTitle}])
@@ -45,47 +45,48 @@ def generate():
     intro = completion.choices[0].message.content
 
     completion = openai.ChatCompletion. create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": "write Literature Survey on "+pTitle}])
+        model="gpt-3.5-turbo", messages=[{"role": "user", "content": "write only Literature Survey on "+pTitle}])
     literature = completion.choices[0].message.content
 
     completion = openai.ChatCompletion. create(
         model="gpt-3.5-turbo", messages=[{"role": "user", "content": "write Conclusion on "+pTitle}])
     conclusion = completion.choices[0].message.content
 
-    completion = openai.ChatCompletion. create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Acknowledgement Based on "+proposedSl}])
-    ack = completion.choices[0].message.content
+    # completion = openai.ChatCompletion. create(
+    #     model="gpt-3.5-turbo", messages=[{"role": "user", "content": "example of basics Acknowledgement as student wroking on mini project Based on "+pTitle}])
+    # ack = completion.choices[0].message.content
+    ack = "hello world"
 
     completion = openai.ChatCompletion. create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Only single Reference Based on "+pTitle}])
+        model="gpt-3.5-turbo", messages=[{"role": "user", "content": "References Based on "+pTitle+".List it in square parentheses numbering."}])
     ref1 = completion.choices[0].message.content
 
-    completion = openai.ChatCompletion. create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Only single Reference different than "+ref1+"Based on "+pTitle}])
-    ref2 = completion.choices[0].message.content
+    # completion = openai.ChatCompletion. create(
+    #     model="gpt-3.5-turbo", messages=[{"role": "user", "content": "single Reference Based on "+pTitle}])
+    # ref2 = completion.choices[0].message.content
+
+    # completion = openai.ChatCompletion. create(
+    #     model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Only single Reference different than "+ref1+","+ref2+"Based on "+pTitle}])
+    # ref3 = completion.choices[0].message.content
 
     completion = openai.ChatCompletion. create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Only single Reference different than "+ref1+","+ref2+"Based on "+pTitle}])
-    ref3 = completion.choices[0].message.content
-
-    completion = openai.ChatCompletion. create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Keywords Related "+pTitle}])
+        model="gpt-3.5-turbo", messages=[{"role": "user", "content": "10 Keywords Related in single line comma format "+pTitle}])
     keywords = completion.choices[0].message.content
 
     document = Document('static/IEEE.docx')  # Open the document
 
     # Access the title paragraph and change its text
     title_paragraph = document.paragraphs[0]
-    title_paragraph.text = pTitle
+    title_paragraph.text = pTitle.upper()
 
     title_paragraph = document.paragraphs[1]
-    title_paragraph.text = author
+    title_paragraph.text = author.title()
 
     title_paragraph = document.paragraphs[5]
     title_paragraph.text = "Abstract- "+abstract
 
     title_paragraph = document.paragraphs[6]
-    title_paragraph.text = 'Keywords- '+keywords
+    title_paragraph.text = 'Keywords- '+keywords.title()
 
     title_paragraph = document.paragraphs[8]
     title_paragraph.text = intro
@@ -106,21 +107,15 @@ def generate():
     title_paragraph.text = conclusion
 
     title_paragraph = document.paragraphs[20]
-    title_paragraph.text = ack
+    title_paragraph.text = "I would like to express my heartfelt gratitude to all the individuals who have contributed and made this mini project of "+pTitle.title()+" a success. First and foremost, I would like to extend my deepest appreciation to my mentor, (mentor’s name), for their invaluable support, guidance, and encouragement throughout the entire duration of this project. Without their guidance, I would not have been able to accomplish this project in a timely and efficient manner. Furthermore, I would like to thank(name of team members) for their hard work and dedication in completing this project. Their inputs and insights have been extremely beneficial in enhancing the overall quality of the project. Last but not least, I would like to express my sincere thanks to the management of the institute(name of the institution) for providing us with all the necessary resources, including space, equipment, and software, and for believing in our potential to complete this project. Once again, I offer my heartfelt thanks to all who have contributed to the successful completion of this mini-project, and I hope that it will prove to be beneficial for the development of the "+pTitle.title()+"."
 
     title_paragraph = document.paragraphs[22]
     title_paragraph.text = ref1
 
-    title_paragraph = document.paragraphs[23]
-    title_paragraph.text = ref2
-
-    title_paragraph = document.paragraphs[24]
-    title_paragraph.text = ref3
-
     document.save('static/output.docx')  # Save the modified document
 
     # Return the generated paper to the user
-    return render_template('result.html', pTitle=pTitle, author=author, problemStatement=problemStatement, proposedSl=proposedSl, abstract=abstract, intro=intro, literature=literature, conclusion=conclusion, ack=ack, result=result, ref1=ref1, ref2=ref2, ref3=ref3)
+    return render_template('result.html', pTitle=pTitle, author=author, problemStatement=problemStatement, proposedSl=proposedSl, abstract=abstract, intro=intro, literature=literature, conclusion=conclusion, ack=ack, result=result)
 
 
 if __name__ == '__main__':
